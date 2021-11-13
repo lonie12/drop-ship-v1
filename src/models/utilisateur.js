d0e9dec0833cfc2b5f317eb8978862f3a028099d
Lonie <rbatoulime@gmail.com>
2021-11-13 20:50:08 +0000

    implement method login an register

--- src/models/utilisateur.js
+++ src/models/utilisateur.js
@@ -1,3 +1,8 @@
+import pkg from "@prisma/client"
+let {PrismaClient} = pkg
+import bcrypt from 'bcrypt'
+
+let prisma = new PrismaClient()
 
 
 export default  {
@@ -10,4 +15,56 @@ export default  {
     },
 
     // Methodes suivants
+    login: async (request, response) => {
+        let {email, password} = request.body
+        if(!(email && password)) {
+            response.status(403).json({
+                error: "All fields is required"
+            })
+        } else {
+            let user = await prisma.user.findFirst({
+                where: {
+                    email: email,
+                }
+            })
+            if(!user || !(await bcrypt.compare(password, user.password))) {
+                response.status(404).json({
+                    error: 'Invalid credentials '
+                })
+            } else {
+                response.status(200).json(user)
+            }
+        }
+    },
+
+    register: async (request, response) => {
+        let {nom, email, telephone, password} = request.body
+        if(!(email, password, telephone, nom)) {
+            response.status(403).json({
+                error: "All fields are required"
+            })
+        } else {
+            let user = await prisma.user.findFirst({
+                where: {
+                    email: email
+                }
+            })
+            if(!user) {
+                user = await prisma.user.create({
+                    data: {
+                        nom: nom,
+                        email: email,
+                        telephone: telephone,
+                        password: await bcrypt.hash(password, 12)
+                    }
+                })
+                response.status(201).json(user)
+
+            } else {
+                response.status(401).json({
+                    error: "User already exists"
+                })
+            }
+        }
+    }
 }
