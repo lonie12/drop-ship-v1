da964af3337e8d42402e4e08a3a4698aeced3acd
godwin <attipkoakrabal@gmail.com>
2021-11-14 19:27:19 +0000

    code de modifier
    j'ai arranger le code de ajouter et et j'ai fait modifier

--- src/models/categorie.js
+++ src/models/categorie.js
@@ -14,47 +14,72 @@ export default  {
     
     //controlleur ajout categorie 
     ajouter: async (request,response) => {
-
-
         // je recupere le libellé de la requete
         let libelle1 = request.body.libelle ;
-
-    
-
-        if(libelle1)
-        {   let Categorie = await prisma.categorie.findFirst({
-                        where:{
-                            libelle: libelle1 
-                        }
-            }) ;
-            
-            if (!Categorie) {
-
+        //on verifie si il a envoyer le libelle
+        if(libelle1){  
+            //je vois si cette categorie exist deja 
+            let Categorie = await prisma.categorie.findFirst({
+                where:{
+                    libelle: libelle1 
+                }
+            });     // si il n y a pas de categorie de ce type je continu 
+                    if (!Categorie) {
                         // crée une categorie 
-                    Categorie = await prisma.categorie.create({
-                        data : {
-                                libelle : libelle1
-                                } 
-                            })
+                        Categorie = await prisma.categorie.create({
+                                    data:{
+                                            libelle : libelle1
+                                    } 
+                        })
+                        return   response.status(200).json(Categorie)
 
-                 return   response.status(200).json(Categorie)
-
-            }
-
-            return response.status(200).json("erreur");
-
-            
+                    }
+                    //erreur : la categorie exist deja 
+                    return response.status(200).json({
+                        error: libelle1+' '+"alredy exist"
+                    });     
         }
-
-        return response.json("error")  ;      
-    
-          
-                
+        // erreur: il n'a pas renseigner libeller 
+        return response.json({
+            error: 'require libelle'
+        })  ;                 
     }, 
 
 
     modifier: async (request , response) =>
     {
+        let categorieid = request.params.categorieid 
+        let {libelle} = request.body
+            if (categorieid) {
+                // on recherche pour voir verifie si la categorie exist
+                let Categorie = await prisma.categorie.findUnique({
+                    where:{
+                        id: categorieid
+                    }
+                });  
+                   // si la categorie exist
+                    if (Categorie!=null) {
+                            // modifier la categoire
+                            Categorie = await prisma.categorie.update({
+                            where:{
+                                id : categorieid
+                            },
+                            data:{
+                                    libelle : libelle
+                            } 
+                        })
+                        //on renvoie la categorie modifié
+                        return response.status(200).json(Categorie)
+                    }
+                    // on renvoie la catorie modifier
+                    return  response.status(200).json({
+                        error: "categorie not exist"
+                    }); 
+
+            }
+            return  response.status(200).json({
+                error: "reseigne categorie ID"
+            });
 
     },
 
